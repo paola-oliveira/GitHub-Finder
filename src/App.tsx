19299d66f3321099a00d3c5f5fa5e8634d40247d
paola-oliveira <paolanatalia_oliveira@outlook.com>
2023-07-28 19:38:20 -0300

    arrumando bug do logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import {Outlet} from "react-router-dom";
 import style from "./App.module.css"; 
+import logo from "../public/Imagens/logo.png";
 
 function App() {
 
   return (
       <div className={style.app}>
-        <img src='../public/Imagens/logo.png' alt='logo do site' className={style.logo}/>
+        <img src={logo} alt='logo do site' className={style.logo}/>
         <Outlet/>
       </div>
   )
